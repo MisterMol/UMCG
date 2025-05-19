f5a99d774bfad114cd91925745ec0856e3d16219
MisterMol <jelled001@gmail.com>
2025-05-19 08:04:37 +0200

    Aanpassingen login systeem

--- django_automation/session_manager/sessions_manager.py
+++ django_automation/session_manager/sessions_manager.py
@@ -12,14 +12,17 @@ def _load_sessions():
     with open(SESSIE_BESTAND, "r", encoding="utf-8") as f:
         return json.load(f)
 
+
 def _save_sessions(data):
     SESSIE_PAD.mkdir(parents=True, exist_ok=True)
     with open(SESSIE_BESTAND, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
 
+
 def get_session(site, username):
     data = _load_sessions()
     return data.get(site, {}).get(username)
+
 
 def save_session(site, username, sessie_data):
     data = _load_sessions()
@@ -30,6 +33,7 @@ def save_session(site, username, sessie_data):
         "timestamp": datetime.now().isoformat()
     }
     _save_sessions(data)
+
 
 def session_exists(site, username):
     data = _load_sessions()
